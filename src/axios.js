ceaad960a08b83062c703c805f4116440f91ae93
heyyybingo <283385508@qq.com>
2021-01-09 21:30:13 +0800

    试试跨域

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,15 @@
 /*
  * @Author: caoqiubin
  * @Date: 2020-11-01 18:36:15
- * @Last Modified by:   caoqiubin
- * @Last Modified time: 2020-11-01 18:36:15
+ * @Last Modified by: caoqiubin
+ * @Last Modified time: 2021-01-09 21:29:54
  */
 //axios模块，导出增强的axios
 import axios from "axios";
-
+let url=process.env.NODE_ENV === 'production' ? 'http://120.79.1.207:8080/aokimall_admin/backAdmin' : 'http://localhost:8083/aokimall_admin/backAdmin',
 const request = axios.create({
   // baseURL: "http://localhost:8088/api",
-  baseURL: "http://localhost:8083/aokimall_admin/backAdmin",
+  baseURL: url,
   timeout: 20000
 });
 
