71ffc57e858b8c410a8e47380ecbdce428eebd4c
gangchiwanxifan <1251656202@qq.com>
2021-01-09 22:08:03 +0800

    活动商品删除功能

--- src/api/activities.js
+++ src/api/activities.js
@@ -10,12 +10,22 @@ const ActivitiesApi = {
   // updateActivities: "/updateActivities",
   updateActivities: "/banner/update",
   // searchActivities: "/searchActivities"
-  searchActivities: "/banner/list"
+  searchActivities: "/banner/list",
+  bannerDetail: "/bannerproduct/listProduct",
+  bannerProduct: "/bannerproduct/list",
+  deleteBannerProduct: "/bannerproduct/delete"
 };
 
 export function selectAllActivities() {
   return request({
     url: ActivitiesApi.selectAllActivities,
+    method: "post"
+  });
+}
+
+export function bannerProduct() {
+  return request({
+    url: ActivitiesApi.bannerProduct,
     method: "post"
   });
 }
@@ -44,9 +54,25 @@ export function deleteActivities(parameter) {
   });
 }
 
+export function deleteBannerProduct(parameter) {
+  return request({
+    url: ActivitiesApi.deleteBannerProduct,
+    method: "post",
+    data: parameter
+  });
+}
+
 export function searchActivities(parameter) {
   return request({
     url: ActivitiesApi.searchActivities,
+    method: "get",
+    params: parameter
+  });
+}
+
+export function bannerDetail(parameter) {
+  return request({
+    url: ActivitiesApi.bannerDetail,
     method: "get",
     params: parameter
   });
--- src/views/Activities/SetGood.vue
+++ src/views/Activities/SetGood.vue
@@ -8,7 +8,7 @@
         }
       "
     />
-    {{ this.$route.query.id }} 设置商品
+    <!-- {{ this.$route.query.id }} 设置商品 -->
     <div class="container">
       <span style="color: #303133"
         ><a-icon type="unordered-list" /> 活动商品</span
@@ -25,91 +25,119 @@
       </a-button>
     </div>
     <!-- 活动商品列表 -->
-    <a-table :columns="columns" :data-source="data">
+    <a-table :columns="columns" :data-source="detail" rowKey="id">
       <a slot="name" slot-scope="text">{{ text }}</a>
-      <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
-      <span slot="tags" slot-scope="tags">
-        <a-tag
-          v-for="tag in tags"
-          :key="tag"
-          :color="
-            tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
-          "
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
+      <span slot="customTitle"><a-icon type="smile-o" /> Id</span>
+
       <span slot="action" slot-scope="text, record">
-        <a>Invite 一 {{ record.name }}</a>
         <a-divider type="vertical" />
-        <a>Delete</a>
+        <a @click="showConfirm(record)">Delete</a>
         <a-divider type="vertical" />
-        <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
       </span>
     </a-table>
   </div>
 </template>
 
 <script>
+import {
+  bannerDetail,
+  bannerProduct,
+  deleteBannerProduct
+} from "@/api/activities.js";
 const columns = [
   {
-    dataIndex: "name",
-    key: "name",
+    dataIndex: "id",
+    key: "id",
     slots: { title: "customTitle" },
-    scopedSlots: { customRender: "name" }
+    scopedSlots: { customRender: "id" }
   },
   {
-    title: "Age",
-    dataIndex: "age",
-    key: "age"
+    title: "商品名称",
+    dataIndex: "name",
+    key: "name"
   },
   {
-    title: "Address",
-    dataIndex: "address",
-    key: "address"
+    title: "价格",
+    dataIndex: "price",
+    key: "price"
   },
   {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    scopedSlots: { customRender: "tags" }
+    title: "简介",
+    key: "details",
+    dataIndex: "details"
   },
   {
-    title: "Action",
+    title: "操作",
     key: "action",
     scopedSlots: { customRender: "action" }
   }
 ];
 
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"]
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"]
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    tags: ["cool", "teacher"]
-  }
-];
 export default {
   data() {
     return {
-      data,
-      columns
+      columns,
+      detail: null,
+      products: null
     };
+  },
+  created() {
+    bannerDetail({ bannerid: this.$route.query.id }).then(res => {
+      this.detail = res.data.list;
+      // console.log(this.detail);
+    });
+    bannerProduct().then(res => {
+      this.products = res.data.page.list;
+      // console.log(this.products);
+    });
+  },
+  methods: {
+    getList() {
+      bannerDetail({ bannerid: this.$route.query.id }).then(res => {
+        this.detail = res.data.list;
+        // console.log(this.detail);
+      });
+    },
+    // 删除活动商品
+    showConfirm(record) {
+      // console.log(this.products);
+      var that = this;
+      for (var item in this.products) {
+        // console.log(item);
+        // console.log(item.productid);
+        // console.log(item.bannerid);
+        // console.log(
+        //   item.productid == record.id && item.bannerid == this.$route.query.id
+        // );
+        if (
+          this.products[item].productid == record.id &&
+          this.products[item].bannerid == this.$route.query.id
+        ) {
+          // console.log(item.id);
+          record = this.products[item].id;
+          break;
+        }
+      }
+      this.$confirm({
+        title: "确定删除该活动商品?",
+        okText: "确认",
+        cancelText: "取消",
+        onOk() {
+          console.log(record);
+          // 删除活动商品
+          deleteBannerProduct([record]).then(res => {
+            if (res.data.code === 0) {
+              console.log("删除成功");
+              that.getList();
+            }
+          });
+          return new Promise((resolve, reject) => {
+            setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+          }).catch(e => console.log(e));
+        },
+        onCancel() {}
+      });
+    }
   }
 };
 </script>
